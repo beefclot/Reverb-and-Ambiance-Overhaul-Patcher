85c19e4ac4fd0d8b8f0a41ef96a83e947e7f1037
beefclot <41597328+beefclot@users.noreply.github.com>
2026-09-07 13:15:36 -0600

    default to skyrim plugin name

--- ReverbandAmbianceOverhaulPatcher/Settings.cs
+++ ReverbandAmbianceOverhaulPatcher/Settings.cs
@@ -5,7 +5,7 @@ namespace ReverbandAmbianceOverhaulPatcher
     public class Settings
     {
         [SynthesisSettingName("Source Plugin")]
-        [SynthesisTooltip("Plugin to forward CELL XCAS Acoustic Space and Region Sound Chances from. Example: Reverb and Ambiance Overhaul - Enderal.esp")]
-        public string SourcePlugin { get; set; } = "Reverb and Ambiance Overhaul - Enderal.esp";
+        [SynthesisTooltip("Plugin to forward CELL XCAS Acoustic Space and Region Sound Chances from. Example: Reverb and Ambiance Overhaul - Skyrim.esp")]
+        public string SourcePlugin { get; set; } = "Reverb and Ambiance Overhaul - Skyrim.esp";
     }
 }
