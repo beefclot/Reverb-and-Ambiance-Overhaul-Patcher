89edd8b57abb96087481cdf740be5e4663d95a42
beefclot <41597328+beefclot@users.noreply.github.com>
2026-09-07 12:43:18 -0600

    initial commit

---
+++ ReverbandAmbianceOverhaulPatcher/Settings.cs
@@ -0,0 +1,11 @@
+using Mutagen.Bethesda.Synthesis.Settings;
+
+namespace ReverbandAmbianceOverhaulPatcher
+{
+    public class Settings
+    {
+        [SynthesisSettingName("Source Plugin")]
+        [SynthesisTooltip("Plugin to forward CELL XCAS Acoustic Space and Region Sound Chances from. Example: Reverb and Ambiance Overhaul - Enderal.esp")]
+        public string SourcePlugin { get; set; } = "Reverb and Ambiance Overhaul - Enderal.esp";
+    }
+}
